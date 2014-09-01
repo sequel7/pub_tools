53cd8e62dc8c741c8c3258380a5b7d3a311b06d3
sequel7 <ronin7@calima7.net>
2014-09-01 15:49:46 -0400

    recognize eap-tls

--- wireless/proof_packet.py
+++ wireless/proof_packet.py
@@ -9,7 +9,7 @@ import xml.etree.ElementTree as et
 #set up arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-r', '--read', metavar='read', help='The packet dump to read', required=True)
-parser.add_argument('-w', '--write', metavar='write', help='The packet dump to write', required=True)
+parser.add_argument('-w', '--write', metavar='write', help='The packet dump to read', required=True)
 group_target = parser.add_mutually_exclusive_group(required=True)
 group_target.add_argument('-b', '--bssid', metavar='bssid', help='The BSSID to search for')
 group_target.add_argument('-s', '--ssid', metavar='ssid', help='The SSID to search for')
@@ -25,7 +25,7 @@ if ssid:
     if args.netxml:
         netxml = args.netxml
     elif os.path.exists(os.path.splitext(read)[0] + '.netxml'):
-        netxml = os.path.splitext(read)[0] + '.netxml'
+        netxml= os.path.splitext(read)[0] + '.netxml'
     elif os.path.exists(os.path.splitext(read)[0] + '.kismet.netxml'):
         netxml = os.path.splitext(read)[0] + '.kismet.netxml'
     else:
@@ -67,21 +67,24 @@ if ssid:
 
 #read files and write temporary output
 #look for a few packets displaying the SSID
-os.system('tshark -c 5 -r {0} -w {1}.ssid -R \"(wlan.bssid eq {2}) and ((wlan.fc.type_subtype eq 0x08) or '
+os.system('tshark -c 5 -r {0} -w {1}.ssid -Y "(wlan.bssid eq {2}) and ((wlan.fc.type_subtype eq 0x08) or '
           '(wlan.fc.type_subtype eq 0x05)) and not (wlan_mgt.ssid eq \\"\\")" '
           '2>/dev/null'.format(read, write, bssid))
 #look for WPA1/2-PSK
-os.system('tshark -c 10 -r {0} -w {1}.psk -R \"(wlan.bssid eq {2}) and wlan_mgt.rsn.akms.type eq 2\" '
+os.system('tshark -c 10 -r {0} -w {1}.psk -Y \"(wlan.bssid eq {2}) and wlan_mgt.rsn.akms.type eq 2\" '
+          '2>/dev/null'.format(read, write, bssid))
+#look for EAP-TLS
+os.system('tshark -c 10 -r {0} -w {1}.eaptls -Y \"(wlan.bssid eq {2}) and eap.type eq 13\" '
           '2>/dev/null'.format(read, write, bssid))
 #look for PEAP
-os.system('tshark -c 10 -r {0} -w {1}.peap -R \"(wlan.bssid eq {2}) and eap.type eq 25\" '
+os.system('tshark -c 10 -r {0} -w {1}.peap -Y \"(wlan.bssid eq {2}) and eap.type eq 25\" '
           '2>/dev/null'.format(read, write, bssid))
 #look for open network
-os.system('tshark -c 10 -r {0} -w {1}.openauth -R \"(wlan.bssid eq {2}) and ((wlan.fc.type_subtype eq 0x08) or '
+os.system('tshark -c 10 -r {0} -w {1}.openauth -Y \"(wlan.bssid eq {2}) and ((wlan.fc.type_subtype eq 0x08) or '
           '(wlan.fc.type_subtype eq 0x05)) and not (wlan_mgt.tag.number eq 48)\" '
           '2>/dev/null'.format(read, write, bssid))
 
 
 #merge temporary outputs into final output
-os.system('mergecap -w {0} {0}.ssid {0}.psk {0}.peap {0}.openauth'.format(write))
-os.system('rm {0}.ssid {0}.psk {0}.peap {0}.openauth'.format(write))
+os.system('mergecap -w {0} {0}.ssid {0}.psk {0}.eaptls {0}.peap {0}.openauth'.format(write))
+os.system('rm {0}.ssid {0}.psk {0}.eaptls {0}.peap {0}.openauth'.format(write))
